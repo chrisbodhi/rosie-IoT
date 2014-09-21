29f74ebcbe9f81ebd58bffab7f43a7f581e91f6f
Chris Boette <cboette@gmail.com>
2014-09-21 12:14:44 -0500

    Add food to main page. Set up main page to act as dashboard for a fictional logged-in user.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,9 +13,12 @@ angular.module('rosieApp')
     $scope.meals = [
       { name: 'Chicken Breast', weight: 100, calories: 172, protein: 21, carbs: 0, fat: 9},
       { name: 'Broccoli', weight: 148, calories: 50, protein: 4.2, carbs: 10, fat: 0.6},
-      { name: 'Mashed Potatoes', weight: 242, calories: 214, protein: 4, carbs: 35, fat: 7},
-      { name: 'Gravy, Chicken', weight: 238, calories: 188, protein: 4.6, carbs: 13, fat: 14},
-      { name: 'Vanilla Ice Cream', weight: 66, calories: 137, protein: 2.3, carbs: 16, fat: 7}
+      { name: 'Mashed Potatoes', weight: 242, calories: 214, protein: 4, carbs: 35, fat: 7}
     ];
+
+    $scope.addFood = function () {
+      $scope.meals.push({name: $scope.foodThing});
+      $scope.foodThing = '';
+    }
 
   });
